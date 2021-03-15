7dcfa08705650f87838974f64546e26b7e1de22a
noobs-creation <sidhudevil50@gmail.com>
2021-03-15 08:01:07 +0530

    trial

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express  = require("express");
+/*const express  = require("express");
 const app = express();
 // const ejs = require("ejs");
 
@@ -47,4 +47,41 @@ app.post('/submitForm', function(req, res){
 // });
 
 //starts server on port 3300
-server.listen(process.env.PORT||3300)
+server.listen(process.env.PORT||3300)*/
+
+const express  = require("express");
+const app = express();
+const ejs = require("ejs");
+
+const bodyParser = require("body-parser");
+const http = require("http");
+const port = process.env.PORT || 3300;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+
+
+const links = [];
+
+
+app.get('/', function(req, res){
+   res.render('landing');
+});
+
+app.get('/:room', function(req, res){
+  // console.log(links)
+  var roomId = req.params.room;
+
+  res.render('room', {roomId: roomId})
+});
+
+app.post('/submitForm', function(req, res){
+    var link = req.body.customLink;
+    res.redirect('/'+link);
+});
+
+app.listen(port, function (){
+  console.log("Server running");
+});
+
